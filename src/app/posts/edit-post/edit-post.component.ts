cd2cd3ff4018a5881a38c17f211ed58831d27af3
GeorgiPetrov23 <georgip114@gmail.com>
2025-08-15 19:44:29 +0300

    Creating components for every page

---
+++ src/app/posts/edit-post/edit-post.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-post',
+  imports: [],
+  templateUrl: './edit-post.component.html',
+  styleUrl: './edit-post.component.css',
+  standalone: true
+})
+export class EditPostComponent {
+
+}
---
+++ src/app/user/user-profile/user-profile.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-profile',
+  imports: [],
+  templateUrl: './user-profile.component.html',
+  styleUrl: './user-profile.component.css',
+  standalone: true
+})
+export class UserProfileComponent {
+
+}
