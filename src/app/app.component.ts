27e54bbd23f9a334e27aaf19be108f556e0a60d4
GeorgiPetrov23 <georgip114@gmail.com>
2025-08-19 15:58:04 +0300

    Creating CSS, header and footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ErrorPageComponent } from "./error-page/error-page.component";
+import { HeaderComponent } from "./core/header/header.component";
+import { LoginComponent } from "./user/login/login.component";
+import { FooterComponent } from "./core/footer/footer.component";
+import { HomeComponent } from "./home/home.component";
+import { CatalogComponent } from "./posts/catalog/catalog.component";
+import { CreatePostComponent } from "./posts/create-post/create-post.component";
+import { DetailsComponent } from "./posts/details/details.component";
+import { EditPostComponent } from "./posts/edit-post/edit-post.component";
+import { RegisterComponent } from "./user/register/register.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, FooterComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   standalone: true
