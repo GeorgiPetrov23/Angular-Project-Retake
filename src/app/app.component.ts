127770cf5bdbfefde45cad71e39614e860d9e3bf
GeorgiPetrov23 <georgip114@gmail.com>
2025-08-19 18:02:40 +0300

    Making routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ErrorPageComponent } from "./error-page/error-page.component";
-import { HeaderComponent } from "./core/header/header.component";
-import { LoginComponent } from "./user/login/login.component";
-import { FooterComponent } from "./core/footer/footer.component";
-import { HomeComponent } from "./home/home.component";
-import { CatalogComponent } from "./posts/catalog/catalog.component";
-import { CreatePostComponent } from "./posts/create-post/create-post.component";
-import { DetailsComponent } from "./posts/details/details.component";
-import { EditPostComponent } from "./posts/edit-post/edit-post.component";
-import { RegisterComponent } from "./user/register/register.component";
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, FooterComponent, CatalogComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   standalone: true
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './user/login/login.component';
+import { RegisterComponent } from './user/register/register.component';
+import { UserProfileComponent } from './user/user-profile/user-profile.component';
+import { CatalogComponent } from './posts/catalog/catalog.component';
+import { DetailsComponent } from './posts/details/details.component';
+import { CreatePostComponent } from './posts/create-post/create-post.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+
+    // User routing
+    { path: 'login', component: LoginComponent, },
+    { path: 'register', component: RegisterComponent,},
+    { path: 'profile', component: UserProfileComponent, },
+
+    //Recipe routing
+    {path: 'recipes', children: [
+        {path: '', component: CatalogComponent},
+        {path: ':recipeId', component: DetailsComponent}
+    ]},
+    {path: 'create', component: CreatePostComponent},
+
+    {path: '404', component: ErrorPageComponent},
+    {path: '**', redirectTo: '/404'}
+
+];
