3f9fa44431f0ef3885c912d24419f9fb1e6bb128
GeorgiPetrov23 <georgip114@gmail.com>
2025-08-20 23:43:22 +0300

    fixing bugs

--- src/app/utils/ingredients-format.validator.ts
+++ src/app/utils/ingredients-format.validator.ts
@@ -1,7 +1,8 @@
 import { ValidatorFn } from "@angular/forms";
 
 export function ingredientsValidator(ingredientsControlName: string): ValidatorFn {
-    const regExp = new RegExp('^[A-Za-z]+(, [A-Za-z]+)*$');
+    const regExp = new RegExp('^[A-Za-z]+ - [1-9]+g(, [a-zA-z]+ - [1-9]+g)*$');
+    // '^[A-Za-z]+ - [0-9]+g.(\\n [A-Za-z]+ - [0-9]+g.)*$'
     return(control) => {
         const ingredientsFormControl = control.get(ingredientsControlName)
         const isValid = control.value === '' || regExp.test(control.value);
